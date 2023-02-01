7e006740e1f018d35c99eb44a88d00b21bd3da54
Mriganka <mrigankapauloracle@gmail.com>
2023-02-01 15:54:37 +0530

    Second Commit

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 
 if (loveScore === 100) {
-  document.write("<h1>The love score between "+a+" and "+b+" is " + loveScore + "%<br><br>" + " Congratulations!<br> You are made for each other!</h1>");
+  document.write("<h1>The love score between "+a.toUpperCase()+" and "+b.toUpperCase()+"<br>is " + loveScore + "%<br><br>" + " Congratulations!<br> You are made for each other!</h1>");
 } else if (loveScore > 70) {
-  document.write("<h1>The love score between "+a+" and "+b+" is " + loveScore + "%<br><br>" + " Time to know each other more deeply!</h1>");
+  document.write("<h1>The love score between "+a.toUpperCase()+" and "+b.toUpperCase()+"<br>is " + loveScore + "%<br><br>" + " Time to know each other more deeply!</h1>");
 } else if (loveScore > 30) {
-  document.write("<h1>The love score between "+a+" and "+b+" is " + loveScore + "%<br><br>" + " Keep going!</h1>");
+  document.write("<h1>The love score between "+a.toUpperCase()+" and "+b.toUpperCase()+"<br>is " + loveScore + "%<br><br>" + " Keep Going!</h1>");
 } else {
-  document.write("<h1>The love score between "+a+" and "+b+" is " + loveScore + "%<br><br>" + " Sorry! find another one XD:)</h1>");
+  document.write("<h1>The love score between "+a.toUpperCase()+" and "+b.toUpperCase()+"<br>is " + loveScore + "%<br><br>" + " Sorry! find another one XD:)</h1>");
 }
