eb5a9e0ea7c44cb3c4a85d453dd072b6b254ae8a
Mriganka <mrigankapauloracle@gmail.com>
2023-02-01 13:16:07 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var a=prompt("Enter Your name");
+var b=prompt("Enter Your Crush's Name");
+
+let loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore === 100) {
+  document.write("<h1>The love score between "+a+" and "+b+" is " + loveScore + "%<br><br>" + " Congratulations!<br> You are made for each other!</h1>");
+} else if (loveScore > 70) {
+  document.write("<h1>The love score between "+a+" and "+b+" is " + loveScore + "%<br><br>" + " Time to know each other more deeply!</h1>");
+} else if (loveScore > 30) {
+  document.write("<h1>The love score between "+a+" and "+b+" is " + loveScore + "%<br><br>" + " Keep going!</h1>");
+} else {
+  document.write("<h1>The love score between "+a+" and "+b+" is " + loveScore + "%<br><br>" + " Sorry! find another one XD:)</h1>");
+}
